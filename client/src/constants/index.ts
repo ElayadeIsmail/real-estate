c348eeeae43fd0cafe3c1c020816bd12a905c965
essofyany <soufyani.bilal@gmail.com>
2022-09-19 22:17:08 +0100

    init remix app with blue-stack template

--- client/src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-export const COOKIES = {
-    AUTH_TOKEN: 'Auth.token',
-};
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,37 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            container: {
-                center: true,
-                padding: {
-                    DEFAULT: '1rem',
-                    sm: '2rem',
-                    lg: '4rem',
-                    xl: '5rem',
-                    '2xl': '6rem',
-                },
-            },
-            boxShadow: {
-                card: '4px 16px 32px rgba(70, 87, 147, 0.12)',
-            },
-            colors: {
-                primary: '#0190FF',
-                primaryDark: '#242748',
-                secondary: '#F1C751',
-                dark: '#101010',
-                gray50: '#F1F1F1',
-                gray100: '#F8F8F8',
-                gray150: '#E9E9E9',
-                gray200: '#EBEBEB',
-                gray500: '#6F6F6F',
-                gray800: '#999999',
-            },
-            fontFamily: {
-                sans: ['"Urbanist"', 'sans-serif'],
-            },
-        },
-    },
-    plugins: [],
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
