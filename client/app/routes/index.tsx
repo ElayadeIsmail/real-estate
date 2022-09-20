0ae097c65a5aefe62dc27a7feadfa062f0ced5f8
bilal essofyany <b.essofyany@harmony-technology.net>
2022-09-20 15:53:19 +0100

    create Header component

--- client/app/routes/index.tsx
+++ client/app/routes/index.tsx
@@ -1,7 +1,12 @@
+import { Header } from "~/components/layout/Header";
+
 export default function Index() {
   return (
-    <div className="">
-      <h1 className="underline decoration-amber-600">Welcome to Remix</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1 className="underline decoration-amber-600">Welcome to Remix</h1>
+      </div>
+    </>
   );
 }
